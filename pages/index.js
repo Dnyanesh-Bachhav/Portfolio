5601a24c95ba0b279c19c179aa6355920f2eccab
Dnyanesh Bachhav <dnyaneshbachhav2002@gmail.com>
2022-07-20 04:55:21 -0700

    adding components folder

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Header from '../components/header'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h1 className='text-3xl text-lime-500 text-bold' >Hello World...!!!</h1>
+    <div>
+      <h1 className='text-3xl text-lime-500 text-bold'>Hello World...!!!</h1>
+      <Header/>
     </div>
   )
 }
