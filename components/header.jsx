a257a7efcb20fbcfe33478b44a59317f275c474a
Dnyanesh Bachhav <dnyaneshbachhav2002@gmail.com>
2022-07-22 05:31:33 -0700

    Adding header styles

--- components/header.jsx
+++ components/header.jsx
@@ -1,43 +1,44 @@
+import styles from "../styles/Header.module.css";
 function Header(){
     return(
         <div>
-            <header className="header" >
-                <nav>
+            <header className={styles.header}>
+                <nav className={styles.nav} >
                     <a href="#" className="nav__logo">Dnyanesh Bachhav</a>
-                    <div className="nav__menu" >
-                        <ul className="nav__list grid">
-                            <li>
-                                <a href="#home">
-                                    <i class="uil uil-estate"></i>
+                    <div className={styles.nav__menu}>
+                        <ul className={styles.Header}>
+                            <li className={styles.nav__item}>
+                                <a href="#home" className={styles.nav__link}>
+                                    <i class="uil uil-estate" className={styles.nav__icon}></i>
                                 </a>
                                 Home
                             </li>
-                            <li>
-                                <a href="#About">
-                                    <i class="uil uil-user"></i>
+                            <li className={styles.nav__item}>
+                                <a href="#About" className={styles.nav__link}>
+                                    <i class="uil uil-user" className={styles.nav__icon}></i>
                                 </a>
                                 About
                             </li>
-                            <li>
-                                <a href="#skills">
-                                    <i class="uil uil-file-alt"></i>
+                            <li className={styles.nav__item}>
+                                <a href="#skills" className={styles.nav__link}>
+                                    <i class="uil uil-file-alt" className={styles.nav__icon}></i>
                                 </a>
                                 Skills
                             </li>
-                            <li>
-                                <a href="#portfolio">
-                                    <i class="uil uil-scenery"></i>
+                            <li className={styles.nav__item}>
+                                <a href="#portfolio" className={styles.nav__link}>
+                                    <i class="uil uil-scenery" className={styles.nav__icon}></i>
                                 </a>
                                 Portfolio
                             </li>
-                            <li>
-                                <a href="#contact">
-                                    <i class="uil uil-message"></i>
+                            <li className={styles.nav__item}>
+                                <a href="#contact" className={styles.nav__link}>
+                                    <i class="uil uil-message" className={styles.nav__icon}></i>
                                 </a>
                                 Contact
                             </li>
                         </ul>
-                        <i class="uil uil-times" id="nav-close"></i>
+                        <i class="uil uil-times" id="nav-close" className={styles.nav__icon}></i>
                     </div>
                     <div className="nav__btns">
                         <div className="nav__toggle" id="nav-toggle">
