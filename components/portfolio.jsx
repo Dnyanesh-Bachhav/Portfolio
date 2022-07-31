d821a0f77f28b8e8e65105fa65e4ba6839a91b7f
Dnyanesh Bachhav <dnyaneshbachhav2002@gmail.com>
2022-07-31 18:46:27 +0530

    Adding Portfolio section

---
+++ components/portfolio.jsx
@@ -0,0 +1,9 @@
+function Portfolio(){
+    return(
+        <section className="section" id="portfolio">
+            <h2 className="section__title">Portfolio</h2>
+            <span className="section__subtitle">Projects I have completed</span>
+        </section>
+    );
+}
+export default Portfolio;
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import About from '../components/about'
 import Contact from '../components/contact'
 import Header from '../components/header'
 import Info from '../components/info'
+import Portfolio from '../components/portfolio'
 import Skills from '../components/skills'
 
 export default function Home() {
@@ -12,7 +13,9 @@ export default function Home() {
       <Info/>
       <About/>
       <Skills/>
+      <Portfolio/>
       <Contact/>
+
     </div>
   )
 }
