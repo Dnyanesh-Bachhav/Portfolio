437058d655f2f582febd2909e2e420f3192dcf40
Dnyanesh Bachhav <dnyaneshbachhav2002@gmail.com>
2022-08-20 10:35:35 +0530

    Adding skills...

--- components/constants.js
+++ components/constants.js
@@ -6,6 +6,8 @@ import TAILWIND from "../assets/images/Skills/Tailwind.png";
 import NODE_JS from "../assets/images/Skills/Node_JS.png";
 import React_Logo from '../assets/images/Skills/React_Logo.png';
 import GRAPHQL from "../assets/images/Skills/GraphQL.png";
+import JAVA from "../assets/images/Skills/Java.png";
+import FIREBASE from "../assets/images/Skills/Firebase.png";
 
 const STUDENT_NAME = "Dnyanesh Bachhav";
 const FIRST_NAME = "Dnyanesh";
@@ -48,39 +50,57 @@ const SKILLS = [
         id: 1,
         name: "HTML",
         img_src: HTML,
+        color_class: "shadow-rose-400"
     },
     
     {
         id: 2,
         name: "CSS",
         img_src: CSS,
+        color_class: "shadow-blue-400"
     },
     {
         id: 3,
         name: "Javascript",
         img_src: JAVASCRIPT,
+        color_class: "shadow-orange-500"
     },
     {
         id: 4,
         name: "React JS",
         img_src: React_Logo,
+        color_class: "shadow-cyan-400"
     },
     {
         id: 5,
         name: "Node JS",
         img_src: NODE_JS,
+        color_class: "shadow-lime-400"
     },
     {
         id: 6,
         name: "Tailwind",
         img_src: TAILWIND,
+        color_class: "shadow-cyan-400"
     },
     {
         id: 7,
         name: "GraphQL",
         img_src: GRAPHQL,
+        color_class: "shadow-pink-400"
     },
-    
+    {
+        id: 8,
+        name: "Java",
+        img_src: JAVA,
+        color_class: "shadow-orange-500"
+    },
+    {
+        id: 9,
+        name: "Firebase",
+        img_src: FIREBASE,
+        color_class: "shadow-amber-600"
+    }
 ];
 
 export {STUDENT_NAME,ABOUT_DESCRIPTION,FIRST_NAME,MAIN_ROLE,LINKEDIN_LINK,YOUTUBE_LINK,GITHUB_LINK,INSTAGRAM_LINK,PHONE_NUMBER,EMAIL,LOCATION,LOGO,BLOB_IMG,PROJECTS,SKILLS,INFO_DESCRIPTION};
--- components/skills.jsx
+++ components/skills.jsx
@@ -10,12 +10,13 @@ function Skills(){
       <div className="flex items-center justify-center px-8 py-16 text-center">
 
         <div className="grid w-full grid-cols-1 items-center justify-around gap-0 sm:grid-cols-2 gap-0 lg:grid-cols-3 gap-8">
-          {SKILLS.map(({ id,name,img_src }) => (
+          {SKILLS.map(({ id,name,img_src,color_class }) => (
             <div
               key={id}
-              className="flex place-content-center sm:flex flex-row items-center justify-center p-6 gap-0  lg:flex-row gap-6 lg:gap-0 self-center items-center justify-between w-4/5 p-5  shadow-lg rounded-xl hover:scale-105 ease-in duration-300
-              odd:shadow-rose-400 even:shadow-blue-400"
+              className={`flex place-content-center sm:flex flex-row items-center justify-center p-6 gap-0  lg:flex-row gap-6 lg:gap-0 self-center items-center justify-between w-4/5 p-5  shadow-lg rounded-xl hover:scale-105 ease-in duration-300 ${color_class}
+              `}
             >
+              {/* odd:shadow-rose-400 even:shadow-blue-400 */}
               <Image src={img_src} width="48px" height="48px" className="object-contain" alt={name} />
               <h3 className="font-light">{name}</h3>
             </div>
