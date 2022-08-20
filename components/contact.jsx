68ccf1d2797305393161292f3d4a511093246c6a
Dnyanesh Bachhav <dnyaneshbachhav2002@gmail.com>
2022-08-20 23:01:04 +0530

    Fetching .env variables

--- components/contact.jsx
+++ components/contact.jsx
@@ -4,11 +4,11 @@ import {PHONE_NUMBER,EMAIL, LOCATION} from '../components/constants';
 import {EMAIL_JS_SERVICE_ID,EMAIL_JS_TEMPLATE_ID,EMAIL_JS_PUBLIC_KEY} from '../EMAIL_JS';
 
 
-function Contact(){
+function Contact(props){
     const form = useRef();
     const sendMail = (e)=>{
         e.preventDefault();
-        emailjs.sendForm(EMAIL_JS_SERVICE_ID, EMAIL_JS_TEMPLATE_ID, form.current, EMAIL_JS_PUBLIC_KEY)
+        emailjs.sendForm( props.EMAIL_JS_SERVICE_ID,  props.EMAIL_JS_TEMPLATE_ID, form.current,  props.EMAIL_JS_PUBLIC_KEY)
         .then((result)=>{
              console.log("Result: "+result.text);
         },(error)=>{
@@ -83,5 +83,17 @@ function Contact(){
         </section>
     );
 }
+
+export async function getServerSideProps(context) {
+    return {
+      props: {
+        EMAIL_JS_SERVICE_ID: process.env.EMAIL_JS_SERVICE_ID,
+        EMAIL_JS_TEMPLATE_ID: process.env.EMAIL_JS_TEMPLATE_ID,
+        EMAIL_JS_PUBLIC_KEY: process.env.EMAIL_JS_PUBLIC_KEY,
+
+      }, // will be passed to the page component as props
+    }
+  }
+
 
 export default Contact;
--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import Info from '../components/info'
 import Portfolio from '../components/portfolio'
 import Skills from '../components/skills'
 
-export default function Home() {
+export default function Home(props) {
   return (
     <div>
       <main className='main'>
@@ -15,10 +15,19 @@ export default function Home() {
       <About/>
       <Skills/>
       <Portfolio/>
-      <Contact/>
+      <Contact EMAIL_JS_SERVICE_ID={props.EMAIL_JS_SERVICE_ID} EMAIL_JS_TEMPLATE_ID={props.EMAIL_JS_TEMPLATE_ID} EMAIL_JS_PUBLIC_KEY={props.EMAIL_JS_PUBLIC_KEY}  />
       <Footer/>
       </main>
     </div>
   )
 }
 
+export async function getServerSideProps(context) {
+  return {
+    props: {
+            EMAIL_JS_SERVICE_ID: process.env.EMAIL_JS_SERVICE_ID,
+            EMAIL_JS_TEMPLATE_ID: process.env.EMAIL_JS_TEMPLATE_ID,
+            EMAIL_JS_PUBLIC_KEY: process.env.EMAIL_JS_PUBLIC_KEY,
+  }
+}
+}
