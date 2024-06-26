25177629fd8cffc071e58a511f535721f441bfa3
Dnyanesh Bachhav <dnyaneshbachhav2002@gmail.com>
2024-06-26 11:41:59 +0530

    Packages upadted...

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: false,
+  swcMinify: true,
   // webpack(config) {
   //   config.module.rules.push({
   //     test: /\.svg$/,
